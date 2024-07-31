3d2b8b599007b6d65d4a11e17c5417f86068002f
H4XOR_ <hakroot07@gmail.com>
2024-08-01 01:01:14 +0200

    create func processHex

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+	"os"
+	"strings"
+)
 
+func main() {
+	var input string
+	fmt.Scanln(&input)
+	input = strings.ToLower(input)
+	if input == "stop" {
+		os.Exit(2)
+	}
+	i := new(big.Int)
+	i.SetString(input, 16)
+}
+
+func processHex(hexStr string) string {
+	return strings.TrimPrefix(hexStr, "0x")
 }
